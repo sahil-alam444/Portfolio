3b2a7f0a1b2474288c5edc75fa735fab31969267
sahil-2526 <sa400919@gmail.com>
2025-06-17 16:10:31 +0530

    update

--- src/assets/pages/components/Footer.jsx
+++ src/assets/pages/components/Footer.jsx
@@ -23,14 +23,14 @@ const Footer = () => {
             <div className='m-auto max-w-[76vw] pt-28 pb-72'>
                 <h3 className='text-8xl'>Reach Out to Me</h3>
                 <div className='mt-28 flex justify-around flex-wrap gap-x-36 gap-y-16 w-4/5 mx-auto'>
-                    <SocialCard src='/social/gmail.png' name='Gmail' click={mail} />
+                    <SocialCard src='/social/gmail.png' name='Gmail' link='mailto:sa400919@gmail.com' />
                     <SocialCard src='/social/facebook.png' name='Facebook' link='https://www.facebook.com/sahil.alam.14289210' />
                     <SocialCard src='/social/instagram.png' name='Instagram' link='https://www.instagram.com/ig_rivage' />
                     <SocialCard src='/social/linkedin.png' name='LinkedIn' link='https://www.linkedin.com/in/sahil-alam444' />
                     <SocialCard src='/social/github.png' name='Github' link='https://www.github.com/sahil-alam444' />
                 </div>
             </div>
-            <div className="text-center py-10 border-t-2 border-white">&copy;2024 ALL RIGHTS RESERVED | WEBSITE CREATED & MAINTAINED BY SAHIL ALAM</div>
+            <div className="text-center py-10 border-t-2 border-white">&copy;{new Date().getFullYear()} ALL RIGHTS RESERVED | WEBSITE CREATED & MAINTAINED BY SAHIL ALAM</div>
         </footer>
     )
 }
