88ca46b005253e6e253d916c850771de8f2ef079
sahil-2526 <sa400919@gmail.com>
2025-11-05 17:57:30 +0530

    project initialized

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata = {
+	title: "Portfolio | Sahil Alam",
+	description: "Portfolio website of Sahil Alam, a software developer specializing in web development, showcasing projects and skills.",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				{children}
+			</body>
+		</html>
+	);
+}
